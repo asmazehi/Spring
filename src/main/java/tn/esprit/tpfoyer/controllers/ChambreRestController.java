de1d35e405acc55e013a377bd09047adf64ff501
Asma Zehi <asmazehi@gmail.com>
2025-02-17 09:51:55 +0100

    ajout tag chambre

--- src/main/java/tn/esprit/tpfoyer/controllers/ChambreRestController.java
+++ src/main/java/tn/esprit/tpfoyer/controllers/ChambreRestController.java
@@ -1,5 +1,6 @@
 package tn.esprit.tpfoyer.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/chambre")
+@Tag(name = "Gestion Chambre")
 
 public class ChambreRestController {
     @Autowired
