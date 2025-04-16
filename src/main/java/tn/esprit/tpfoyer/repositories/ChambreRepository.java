c306b8ca63704025b23364a739b963ad76f5c865
Asma Zehi <asmazehi@gmail.com>
2025-04-16 11:41:22 +0100

    ex1/ex2 et service1 du rendu

--- src/main/java/tn/esprit/tpfoyer/repositories/ChambreRepository.java
+++ src/main/java/tn/esprit/tpfoyer/repositories/ChambreRepository.java
@@ -2,6 +2,7 @@ package tn.esprit.tpfoyer.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import tn.esprit.tpfoyer.entities.Bloc;
 import tn.esprit.tpfoyer.entities.Chambre;
 import tn.esprit.tpfoyer.entities.TypeChambre;
 
@@ -11,6 +12,7 @@ import java.util.List;
 public interface ChambreRepository extends JpaRepository<Chambre, Long> {
     List<Chambre> findAllByTypeChambre(TypeChambre tc);
     Chambre findChambreByNumeroChambre(Long num);
+    List<Chambre> findByBlocs(Bloc bloc);
     // No need to code CRUD here. It is already provided by the Spring Data JPA interfaces:
     // - CrudRepository
     // - PagingAndSortingRepository
--- src/main/java/tn/esprit/tpfoyer/services/ChambreService.java
+++ src/main/java/tn/esprit/tpfoyer/services/ChambreService.java
@@ -2,23 +2,43 @@ package tn.esprit.tpfoyer.services;
 
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import tn.esprit.tpfoyer.entities.Bloc;
 import tn.esprit.tpfoyer.entities.Chambre;
 import tn.esprit.tpfoyer.entities.Reservation;
 import tn.esprit.tpfoyer.entities.TypeChambre;
+import tn.esprit.tpfoyer.repositories.BlocRepository;
 import tn.esprit.tpfoyer.repositories.ChambreRepository;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+
 
 public class ChambreService implements IChambreService {
+
     @Autowired
     ChambreRepository chambreRepository;
+    @Autowired
+    private BlocRepository blocRepository;
+
     @Override
-    public List<Chambre> retrieveAllChambres() {return chambreRepository.findAll();}
+    @Scheduled(cron = "*/10 * * * * *") // Toutes les 10 secondes
+    public List<Chambre> retrieveAllChambres() {
+        List<Chambre> listC = chambreRepository.findAll();
+        for (Chambre c: listC) {
+            log.info("Chambre - ID: {}, Numero: {}, Type: {}",
+                    c.getIdChambre(), c.getNumeroChambre(), c.getTypeC());
+        }
+        return listC;
+    }
     @Override
     public Chambre retrieveChambre(Long chambreId) {
         return chambreRepository.findById(chambreId).get();
@@ -48,5 +68,24 @@ public class ChambreService implements IChambreService {
         return chambreRepository.save(ch);
 
     }
+    @Scheduled(fixedRate = 60000) // toutes les minutes
+    public void listeChambresParBloc() {
+        List<Bloc> blocs = blocRepository.findAll();
 
+        for (Bloc bloc : blocs) {
+            log.info("Bloc => {} ayant une capacité {}", bloc.getNomBloc(), bloc.getCapaciteBloc());
+            List<Chambre> chambres = chambreRepository.findByBlocs(bloc);
+
+            if (chambres.isEmpty()) {
+                log.info("Pas de chambre disponible dans ce bloc");
+            } else {
+                log.info("La liste des chambres pour ce bloc:");
+                for (Chambre chambre : chambres) {
+                    log.info("NumChambre: {} type: {}", chambre.getNumeroChambre(), chambre.getTypeC());
+                }
+            }
+
+            log.info("******************");
+        }
+    }
 }
